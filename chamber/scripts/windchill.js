7feb8f8a2a75156a94c69cc7573bd925c7d0b700
marylilian1618 <hiimlyyofgmail.com>
2024-02-19 18:23:23 -0500

    Chamber home

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,16 @@
+function calculateWindChill() {
+    var temperatureInput = document.getElementById('temperature').value;
+    var windSpeedInput = document.getElementById('windSpeed').value;
+    //condicional 
+    if (temperatureInput <= 50 && windSpeedInput > 3.0) {
+        var windChill = calculateWindChillValue(temperatureInput, windSpeedInput);
+        document.getElementById('windChillResult').innerText = 'Wind Chill: ' + windChill.toFixed(2) + ' m/s';
+    } else {
+        //No se cumple el si
+        document.getElementById('windChillResult').innerText = 'N/A';
+    }
+}
+
+function calculateWindChillValue(temperature, windSpeed) {
+    return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+}
