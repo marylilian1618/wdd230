3a2209dd60532760d73c9bc776c7ce96ae9aef29
marylilian1618 <hiimlyyofgmail.com>
2024-02-24 20:00:02 -0500

    visits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -8,3 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     lastModifiedElement.textContent = `Last modified: ${lastModifiedDate.toLocaleDateString('en-US', options)}`;
 });
+
+/*-----------------*/
+const visitsDisplay = document.querySelector(".visits");
+/*will show the number of visits*/
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+//now determine the visits
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+
+} else {
+    visitsDisplay.textContent = "This is your first visit. WELCOME ❤️";
+
+}
+//increment the number of visits by 1
+numVisits++
+//stire the new visit total into localstorage
+localStorage.setItem("numVisits-ls", numVisits);
