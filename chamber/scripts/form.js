6b7c479398fa2d79bcb81356d82e9aabc5f0b9d7
marylilian1618 <hiimlyyofgmail.com>
2024-03-07 17:54:28 -0500

    Join

---
+++ chamber/scripts/form.js
@@ -0,0 +1,8 @@
+//----------Date form-------------//
+document.addEventListener("DOMContentLoaded", function () {
+    var datetime = document.getElementById("dtime");
+    if (datetime) {
+        datetime.value = Date.now();
+    }
+})
+
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -10,19 +10,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /*-----------------*/
-const visitsDisplay = document.querySelector(".visits");
-/*will show the number of visits*/
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-//now determine the visits
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
-
-} else {
-    visitsDisplay.textContent = "This is your first visit. WELCOME ❤️";
-
-}
-//increment the number of visits by 1
-numVisits++
-//stire the new visit total into localstorage
-localStorage.setItem("numVisits-ls", numVisits);
