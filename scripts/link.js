deb977b952e69361ad69dbb3aca684168411c417
marylilian1618 <hiimlyyofgmail.com>
2024-03-14 14:19:58 -0500

    Index js

--- scripts/link.js
+++ scripts/link.js
@@ -1,9 +1,30 @@
 const baseURL = "https://marylilian1618.github.io/wdd230/";
 const linksURL = "https://marylilian1618.github.io/wdd230/data/links.json";
-async function GetLinks() {
+const linkContainer = document.querySelector("#link");
+
+async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-
-
+    displayLinks(data.lessons);
 }
+
+function displayLinks(lessons) {
+    lessons.forEach(lesson => {
+        const lessonHeading = document.createElement('h3');
+        lessonHeading.textContent = `Week ${lesson.lesson}`;
+        linkContainer.appendChild(lessonHeading);
+
+        const ol = document.createElement('ol');
+        lesson.links.forEach(link => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = baseURL + link.url;
+            a.textContent = link.title;
+            li.appendChild(a);
+            ol.appendChild(li);
+        });
+
+        linkContainer.appendChild(ol);
+    });
+}
+getLinks();
