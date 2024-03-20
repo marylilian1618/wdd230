eed70ef0e05a81b7e77e6dfa1f7d5ae939d7ef45
marylilian1618 <hiimlyyofgmail.com>
2024-03-20 15:43:39 -0500

    weather

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7597&lon=6.6467&units=imperial&appid=cabe9a3506f3a25de8dc6f81e852982f';
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);// for testing
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed()}&deg;F`; //no decimals .toFixed()
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', "icon");
+    captionDesc.textContent = desc.toUpperCase()// uppercase string
+
+}
